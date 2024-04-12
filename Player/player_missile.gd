0d83e67d85eb1c18c893a90fd4f55fa4f8231c89
Szymon Bulczak <sbulczak18@gmail.com>
2024-04-12 23:27:33 +0200

    Added destroyable brick

---
+++ Player/player_missile.gd
@@ -0,0 +1,11 @@
+extends "res://Scenes/projectile.gd"
+
+const EFFECT = preload("res://Effect/enemy_death_effect.tscn")
+
+const BRICK_LAYER_BIT =  18
+
+func _on_hitbox_body_entered(_body):
+	if _body.get_collision_layer() == BRICK_LAYER_BIT:
+		_body.queue_free()
+		Utils.instance_scene_on_main(EFFECT, _body.global_position + Vector2(8,8))
+	super._on_hitbox_body_entered(_body)
---
+++ World/tile_map.gd
@@ -0,0 +1,6 @@
+extends TileMap
+
+
+func get_collision_layer():
+	return 2
+
